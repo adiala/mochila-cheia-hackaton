abfe3148a1758874f2f986af0b922299919c9212
adiala <david.adiala@gmail.com>
2021-03-30 18:44:20 -0300

    construção do menu mobile

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+const mobMenu = document.querySelector('#btnHamburguer');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+mobMenu.addEventListener('click', function(){
+    console.log('Teste');
+    if (header.classList.contains('open')) { // Close Menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } 
+    else { // Open Menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+});
